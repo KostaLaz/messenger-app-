80250b20ba6af94676615f4cf65cd4c2293057bd
kosta <kosta.lazarevski89@gmail.com>
2020-11-22 16:57:24 +0100

    model created

--- demo/src/main/java/com/messengerapp/demo/model/Message.java
+++ demo/src/main/java/com/messengerapp/demo/model/Message.java
@@ -1,5 +1,17 @@
 package com.messengerapp.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
     
+    private long id;
+    private String content;
+    private Data created;
+    private String author;
+
 }
