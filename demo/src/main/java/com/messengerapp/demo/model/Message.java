9a3d879c6293ff2186cf4ad4e8638a745aa9e426
kosta <kosta.lazarevski89@gmail.com>
2020-11-22 22:33:38 +0100

    Model created

--- demo/src/main/java/com/messengerapp/demo/model/Message.java
+++ demo/src/main/java/com/messengerapp/demo/model/Message.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Message {
-    
+
     private long id;
     private String content;
-    private Data created;
+    private Date created;
     private String author;
 
+    public Message(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
 }
